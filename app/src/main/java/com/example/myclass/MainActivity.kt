d7a75f3b92c856e518f9d1a85a83c0e498b58e95
Paul ROOS <roos.wac@gmail.com>
2022-10-17 16:01:19 +0200

    test xcode

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myclass
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,26 +14,6 @@ import com.example.myclass.ui.theme.MyClassTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyClassTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyClassTheme {
-        Greeting("Android")
+//        test
     }
 }
