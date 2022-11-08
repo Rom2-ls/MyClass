19442f60f6a9bae69724a0c9e4bf6e0b8380016b
Paul ROOS <roos.wac@gmail.com>
2022-11-08 15:53:10 +0100

    Add signin and signup activity.

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myclass
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        if(firebaseAuth.currentUser != null){
+            println(firebaseAuth.currentUser!!.email)
+            setContentView(R.layout.activity_main)
+        } else {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
