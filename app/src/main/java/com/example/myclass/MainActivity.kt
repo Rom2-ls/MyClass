e664af8dc71d848f6aae137715559ef30ea574a7
romain capolunghi <romain@DESKTOP-HIJDIQJ.localdomain>
2022-11-08 14:15:27 +0100

    update kotlin version

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -1,19 +1,11 @@
 package com.example.myclass
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myclass.ui.theme.MyClassTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        test
+        setContentView(R.layout.activity_main)
     }
 }
