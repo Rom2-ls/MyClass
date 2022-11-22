3d45f79059cc31df6725cea5eaa1db25de842dc6
Paul ROOS <roos.wac@gmail.com>
2022-11-22 15:07:13 +0100

    Ajout du recycler view pour les classes

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.myclass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.Objects
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, DetailCoursActivity::class.java)
+        val intent = Intent(this, ListMatiereActivity::class.java)
         startActivity(intent)
+
     }
 }
