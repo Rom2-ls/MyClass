f393cf96c39e06e030ea6d0ef30e82e49e65bcf0
Paul ROOS <roos.wac@gmail.com>
2022-11-22 11:34:50 +0100

    Ajout de l'activité listing de matières.

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, ListResponsableActivity::class.java)
+        val intent = Intent(this, ListMatiereActivity::class.java)
         startActivity(intent)
     }
 }
