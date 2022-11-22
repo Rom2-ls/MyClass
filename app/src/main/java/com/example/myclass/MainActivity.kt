1333b6c7a886ffb72483bc2a43394af8bea0759e
Paul ROOS <roos.wac@gmail.com>
2022-11-22 09:16:00 +0100

    Add design

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -4,16 +4,25 @@ import android.app.Activity
 import android.content.Intent
 import com.google.firebase.auth.FirebaseAuth
 import android.os.Bundle
+import com.example.myclass.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.buttonLogout.setOnClickListener({
+            firebaseAuth.signOut()
+            finish()
+            startActivity(getIntent())
+        });
 
         if(firebaseAuth.currentUser != null){
             println(firebaseAuth.currentUser!!.email)
-            setContentView(R.layout.activity_main)
+            setContentView(binding.root)
         } else {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
