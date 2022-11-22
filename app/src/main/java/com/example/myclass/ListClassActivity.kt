ba691ed10a8ec6bf96659a3db000c57dcec209a7
Paul ROOS <roos.wac@gmail.com>
2022-11-22 10:15:58 +0100

    Ajout activity ListClass

---
+++ app/src/main/java/com/example/myclass/ListClassActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myclass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListClassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_class)
+    }
+}
