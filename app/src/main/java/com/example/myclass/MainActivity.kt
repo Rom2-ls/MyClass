1e5a7ab948d4d677a3b928883b8f299dcbc6263a
Paul ROOS <roos.wac@gmail.com>
2022-11-29 17:03:51 +0100

    gestion authent

--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -3,36 +3,27 @@ package com.example.myclass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.inflate
 import android.widget.Button
 import com.example.myclass.databinding.ActivityMainBinding
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import java.util.Objects
 import com.google.firebase.auth.FirebaseAuth
-import com.example.myclass.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.buttonLogout.setOnClickListener({
-            firebaseAuth.signOut()
-            finish()
-            startActivity(getIntent())
-        });
+//            firebaseAuth.signOut()
+//            finish()
+//            startActivity(getIntent())
 
         if(firebaseAuth.currentUser != null){
-            println(firebaseAuth.currentUser!!.email)
-            setContentView(binding.root)
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         } else {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
