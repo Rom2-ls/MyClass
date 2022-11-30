3a9e4e06b25325a2d973dc6a291b43cfe38a9c86
romain capolunghi <romain@DESKTOP-HIJDIQJ.localdomain>
2022-11-30 11:58:25 +0100

    ADD creation de classes

--- app/src/main/java/com/example/myclass/CreateClassActivity.kt
+++ app/src/main/java/com/example/myclass/CreateClassActivity.kt
@@ -1,15 +1,34 @@
 package com.example.myclass
 
-import android.R
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myclass.databinding.ActivityCreateClassBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class CreateClassActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateClassBinding
+    private lateinit var database: DatabaseReference
+
+    var currentUser = FirebaseAuth.getInstance().currentUser
+
+
+    data class NewClass(val name: String = "")
+    data class NewStudent(val role: String = "ADMIN")
+
+    private fun createClass(name: String) {
+        database = Firebase.database.reference
+
+        val newclass = NewClass(name);
+        val newstudent = NewStudent()
+        val classId = database.child("Classes").push().key
+        database.child("Classes").child(classId.toString()).setValue(newclass)
+        database.child("Classes").child(classId.toString()).child("Students").child(currentUser!!.uid).setValue(newstudent)
+    }
 
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +36,15 @@ class CreateClassActivity : AppCompatActivity() {
 
         binding = ActivityCreateClassBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btncreateclass.setOnClickListener {
+            val name = binding.nameField.text.toString()
+
+            if (name.isNotEmpty()) {
+                createClass(name)
+                Toast.makeText(applicationContext,"toast after database",Toast.LENGTH_LONG).show()
+                // rediriger vers la page de liste des classes
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myclass/MainActivity.kt
+++ app/src/main/java/com/example/myclass/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Objects
 import com.google.firebase.auth.FirebaseAuth
-import com.example.myclass.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
