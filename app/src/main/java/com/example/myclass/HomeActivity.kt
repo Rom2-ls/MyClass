ff55bf8a6ec04b05ad00dda0f19f5345ef25535f
Paul ROOS <roos.wac@gmail.com>
2022-12-05 12:46:49 +0100

    Ajout de la navbar

--- app/src/main/java/com/example/myclass/HomeActivity.kt
+++ app/src/main/java/com/example/myclass/HomeActivity.kt
@@ -7,15 +7,19 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.content.PackageManagerCompat.LOG_TAG
 import com.example.myclass.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var navView: BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navView = findViewById(R.id.nav_view)
 
         binding.btnJoinClassActivity.setOnClickListener {
             val intent = Intent(this, JoinClassActivity::class.java)
@@ -26,5 +30,25 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, CreateClassActivity::class.java)
             startActivity(intent)
         }
+
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    Log.d("TEST", "switch to acticity home")
+                    true
+                }
+                R.id.navigation_dashboard -> {
+                    Log.d("TEST", "switch to acticity dash")
+                    true
+                }
+                R.id.navigation_parametres -> {
+                    Log.d("TEST", "switch to acticity param")
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+            }
     }
 }
