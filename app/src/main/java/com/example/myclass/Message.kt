557c962ec746d0fa23a540d34aafb22fa869a10f
romain capolunghi <romain@DESKTOP-HIJDIQJ.localdomain>
2022-12-13 02:38:05 +0100

    FIX, CLEAN merge between branch textmessage and master

--- app/src/main/java/com/example/myclass/Message.kt
+++ app/src/main/java/com/example/myclass/Message.kt
@@ -1,23 +1,3 @@
 package com.example.myclass
 
-//<<<<<<< HEAD
-data class Message(val id: String = "",
-                   val message: String = "",
-                    val idStudent: String = "")
-
-
-/*=======
-import java.util.*
-
-object MessageType {
-    const val TEXT = "TEXT"
-    const val image = "IMAGE"
-
-}
-
-interface Message {
-    val time: Date
-    val senderId: String
-    val type: String
-}
->>>>>>> e7dbdff3fd080e94f5413859f66c58d5b68a641f*/
+data class Message(val id: String = "", val message: String = "", val idStudent: String = "")
